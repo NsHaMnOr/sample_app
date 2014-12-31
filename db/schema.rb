14e07748ab48fb109c281add5ad3c21bcfcec35b
Shaheerah <nshamnor941126@gmail.com>
2014-12-31 17:50:37 +0800

    Add password resets & email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224044049) do
-
-  create_table "microposts", force: true do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
+ActiveRecord::Schema.define(version: 20141231090928) do
 
   create_table "users", force: true do |t|
     t.string   "name"
